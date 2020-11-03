ec97f8f0edbf19cba04f47efcf13077dded6bdf6
GoldDever <69146353+GoldDever@users.noreply.github.com>
2020-11-03 20:56:43 +0600

    Удален jpa репозиторий

--- src/main/java/data/controller/FirstController.java
+++ src/main/java/data/controller/FirstController.java
@@ -1,14 +1,11 @@
 package data.controller;
 
 import data.entity.User;
-import data.repository.UserRepository;
 import data.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 public class FirstController {
--- src/main/java/data/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package data.repository;
-
-import data.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository {
-}
